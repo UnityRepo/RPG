208fbdc9e5a181a95c7abc1a03a2fb4a0c899fec
Repo <142061883+UnityRepo@users.noreply.github.com>
2023-08-13 09:32:16 -0400

    polished continue button

--- Assets/Scripts/TBScrolling.cs
+++ Assets/Scripts/TBScrolling.cs
@@ -1,32 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TBScrolling : MonoBehaviour
 {
+    public GameObject BTNCon;
     public GameObject playerText;
     public Text CNTBox;
     public Text CTTBox;
     IEnumerator Start()
     {
+        BTNCon.SetActive(false);
         yield return StartCoroutine(CText("Unknown", "Come on, wake up!", 1,1));
-        yield return new WaitForSeconds(2);
+        yield return StartCoroutine(keyPress(2));
         yield return StartCoroutine(CText("You", "*mmhh*", 0,1));
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        yield return StartCoroutine(keyPress(0));
         yield return StartCoroutine(CText("", "*You wake up*", 2,1));
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        yield return StartCoroutine(keyPress(0));
         yield return StartCoroutine(CText("You", "Who are you?", 0, 1));
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        yield return StartCoroutine(keyPress(0));
         yield return StartCoroutine(CText("Spirit", "I'm Spirit!", 1, 1));
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        yield return StartCoroutine(keyPress(0));
         yield return StartCoroutine(CText("Spirit", "and you are?", 1, 1));
     }
 
 
-    void Update()
+    public IEnumerator keyPress(float waitTime)
     {
-        
+        yield return new WaitForSeconds(waitTime);
+        BTNCon.SetActive(true);
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        BTNCon.SetActive(false);
+
     }
     public IEnumerator CText(string cname, string ctext, int positioning, float baseSpeed)
     {
@@ -39,7 +46,7 @@ public class TBScrolling : MonoBehaviour
         {
             playerText.transform.position = new Vector3(6f, -1.2f, 0f);
             CNTBox.transform.position = new Vector3(1620f, 416f, 0f);
-        } else if (positioning == 2)
+        } else if (positioning == 2) //gone
         {
             playerText.transform.position = new Vector3(-261.2001f, -218.0f, 0.0f);
             CNTBox.transform.position = new Vector3(-261.2001f, -218.0f, 0.0f);
